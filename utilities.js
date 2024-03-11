f57f2e98f6e3bc8b507acf0cf9b3ff0427780d87
kseniya01 <155999972+kseniya01@users.noreply.github.com>
2024-03-11 20:39:07 +0000

    2

--- utilities.js
+++ utilities.js
@@ -1,6 +1,4 @@
-window.onload = function() {
-    ScrollReveal().reveal('.site', {delay: 1000});
-};
+ScrollReveal().reveal('.site', {delay: 1000});
 
 // Standard function 
 function clickOMatic() {
