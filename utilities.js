f2aa17f2553352c0319ebc20c6291c427f0a50a6
kseniya01 <155999972+kseniya01@users.noreply.github.com>
2024-03-11 20:43:25 +0000

    3

--- utilities.js
+++ utilities.js
@@ -1,5 +1,11 @@
 ScrollReveal().reveal('.site', {delay: 1000});
 
+sayHi("User"); // Hello, User
+
+function sayHi(name) {
+  alert( `Hello, ${name}` );
+};
+
 // Standard function 
 function clickOMatic() {
     alert('Hello!');
