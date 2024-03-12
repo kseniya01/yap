11fb52c8d7878cdd0ca01490bac983b9575216d1
kseniya01 <155999972+kseniya01@users.noreply.github.com>
2024-03-12 07:05:12 +0000

    3

--- utilities.js
+++ utilities.js
@@ -11,21 +11,4 @@ window.onload = function() {
     click.addEventListener("click", (event) => {
         clickOMatic();
     });
-};
-
-<button onclick="one()">1</button>
-
-<script>
-// Get the elements with class="column"
-var elements = document.getElementsByClassName("column");
-
-// Declare a "loop" variable
-var i;
-
-// Full-width images
-function one() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "100%";
-  }
 }
-</script>
