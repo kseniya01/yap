359423967698e0857d2ce0ebde4ab0d0fede1d5d
kseniya01 <155999972+kseniya01@users.noreply.github.com>
2024-03-12 06:54:57 +0000

    2

--- utilities.js
+++ utilities.js
@@ -12,3 +12,20 @@ window.onload = function() {
         clickOMatic();
     });
 };
+
+<button onclick="one()">1</button>
+
+<script>
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a "loop" variable
+var i;
+
+// Full-width images
+function one() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "100%";
+  }
+}
+</script>
